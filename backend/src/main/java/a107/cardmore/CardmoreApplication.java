c0de01701851e31514ae381be0bfc76590f49e95
dongyul <comeme0101@naver.com>
2024-09-03 11:00:09 +0900

    feat: init backend initial setting

---
+++ backend/src/main/java/a107/cardmore/CardmoreApplication.java
@@ -0,0 +1,13 @@
+package a107.cardmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardmoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/a107/cardmore/CardmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package a107.cardmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
