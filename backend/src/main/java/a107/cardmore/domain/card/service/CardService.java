45b8e1baabf3b0ca1a5d757590c08c66b9d441fc
dahyunko <kdhgby@naver.com>
2024-09-09 16:17:13 +0900

    temp

--- backend/src/main/java/a107/cardmore/domain/card/service/CardService.java
+++ backend/src/main/java/a107/cardmore/domain/card/service/CardService.java
@@ -1,8 +1,41 @@
 package a107.cardmore.domain.card.service;
 
+import a107.cardmore.domain.card.dto.CardResponseDto;
+import a107.cardmore.domain.user.entity.User;
+import a107.cardmore.domain.user.service.UserModuleService;
+import a107.cardmore.util.api.RestTemplateUtil;
+import a107.cardmore.util.api.dto.card.CardResponseRestTemplateDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class CardService {
+    private final RestTemplateUtil restTemplateUtil;
+    private final UserModuleService userModuleService;
+
+    public List<CardResponseDto> getUserAllCardInfo(String email){
+        User user  = userModuleService.getUserByEmail(email);
+        List<CardResponseRestTemplateDto> cardResponseInfos = restTemplateUtil.inquireSignUpCreditCardList(user.getUserKey());
+
+        List<CardResponseDto> cardResponseDtos = new ArrayList<>();
+
+        // 카드 정보 저장
+        for(CardResponseRestTemplateDto cardResponseRestTemplateDto : cardResponseInfos){
+
+            restTemplateUtil.inquireCreditCardList();
+
+            CardResponseDto cardResponseDto = new CardResponseDto();
+
+            cardResponseDtos.add(cardResponseDto);
+        }
+
+
+
+        return null;
+    }
 
 }
--- backend/src/main/java/a107/cardmore/domain/user/service/UserModuleService.java
+++ backend/src/main/java/a107/cardmore/domain/user/service/UserModuleService.java
@@ -1,6 +1,8 @@
 package a107.cardmore.domain.user.service;
 
+import a107.cardmore.domain.user.entity.User;
 import a107.cardmore.domain.user.repository.UserRepository;
+import a107.cardmore.global.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,7 +13,7 @@ import org.springframework.stereotype.Service;
 public class UserModuleService {
     private final UserRepository userRepository;
 
-    public String getUserKeyByEmail(){
-        return null;
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email).orElseThrow(() -> new BadRequestException("일치하는 회원이 없습니다."));
     }
 }
