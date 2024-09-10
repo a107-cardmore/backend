4755daa69e14837f60fc7dd3d6c21dbec0f19a2c
dongyul <comeme0101@naver.com>
2024-09-10 14:29:34 +0900

    feat: 장소에 따른 카드 추천 로직 구현

--- backend/src/main/java/a107/cardmore/domain/card/repository/CardRepository.java
+++ backend/src/main/java/a107/cardmore/domain/card/repository/CardRepository.java
@@ -5,8 +5,13 @@ import a107.cardmore.domain.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
     List<Card> findAllByCompany(Company company);
+
+    @Query("SELECT c FROM Card c WHERE c.company.user.id = :userId AND c.isSelected = true AND c.limitRemaining > 0")
+    List<Card> findCardsByUserId(@Param("userId") Long userId);
 }
---
+++ backend/src/main/java/a107/cardmore/util/recommend/MapResponseDto.java
@@ -0,0 +1,10 @@
+package a107.cardmore.util.recommend;
+
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+public class MapResponseDto {
+
+}
