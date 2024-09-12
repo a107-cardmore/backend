2dc3b78a094e21f2116f286695ca7e7239f39321
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-09-12 17:12:26 +0900

    feat: 할인 패키지 추가

---
+++ backend/src/main/java/a107/cardmore/domain/discount/controller/DiscountController.java
@@ -0,0 +1,13 @@
+package a107.cardmore.domain.discount.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/discounts")
+@RequiredArgsConstructor
+@Slf4j
+public class DiscountController {
+}
