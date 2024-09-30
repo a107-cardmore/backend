10420e021793b22e544607ed84eaa07be1c9a801
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-09-30 14:45:21 +0900

    feat: 소비 패턴 차트 기능 구현

--- frontend/cardmore/src/components/ExpenditureCard.jsx
+++ frontend/cardmore/src/components/ExpenditureCard.jsx
@@ -1,49 +1,109 @@
 import React from "react";
 import { css } from "@emotion/css";
 
-function ExpenditureCard(){
-  return(
-    <div className={css`
-      display: flex;
-      justify-content: space-between;
-      border-bottom: 1px solid #C6C6C6;
-      padding : 5px;
-    `}>
-      <div className={css`
+function ExpenditureCard({ transactionList = [] }) {
+  console.log(transactionList);
+
+  const displayDate = (dateString) => {
+    // 연, 월, 일을 추출
+    const month = dateString.slice(4, 6); // '09'
+    const date = dateString.slice(6, 8); // '06'
+
+    const formatDate = `${month}-${date}`;
+    return formatDate;
+  };
+
+  return transactionList.length === 0 ? (
+    <div
+      className={css`
         display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-      `}>
-        <div className={css`
-          font-weight: 700;
-          color : #959595;
-          font-size: 1rem;
-        `}>
-          스타벅스 역삼점
-        </div>
-        <div className={css`
+        justify-content: center;
+        padding: 5px;
+      `}
+    >
+      <div
+        className={css`
           display: flex;
-          justify-content: space-between;
-          width : 6.5rem;
-          font-size: 0.9rem;
-        `}>
-          <span className={css`
-            color: #C6C6C6;
-          `}>04.21</span>
-          <span className={css`
-            font-weight: 600;
-            color: #FE4437;
-          `}>국민카드</span>
+          flex-direction: column;
+        `}
+      >
+        <div
+          className={css`
+            font-weight: 700;
+            color: #959595;
+            font-size: 1rem;
+          `}
+        >
+          결제 내역이 존재하지 않습니다.
         </div>
       </div>
-      <div className={css`
-        text-align: center;
-        line-height : 3rem;
-        height: 100%;
-        font-size: 1.2rem;
-        color : #EE3B24;
-      `}>-10,000</div>
     </div>
+  ) : (
+    transactionList.map((transaction, key) => (
+      <div
+        key={key}
+        className={css`
+          display: flex;
+          justify-content: space-between;
+          border-bottom: 1px solid #c6c6c6;
+          padding: 5px;
+        `}
+      >
+        <div
+          className={css`
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+          `}
+        >
+          <div
+            className={css`
+              font-weight: 700;
+              color: #959595;
+              font-size: 1rem;
+            `}
+          >
+            {transaction.merchantName}
+          </div>
+          <div
+            className={css`
+              display: flex;
+              justify-content: flex-start;
+              width: 10.5rem;
+              font-size: 0.9rem;
+            `}
+          >
+            <span
+              className={css`
+                color: #c6c6c6;
+                padding-right: 0.5rem;
+              `}
+            >
+              {displayDate(transaction.transactionDate)}
+            </span>
+            <span
+              className={css`
+                font-weight: 600;
+                color: ${transaction.colorTitle};
+              `}
+            >
+              {transaction.cardName}
+            </span>
+          </div>
+        </div>
+        <div
+          className={css`
+            text-align: center;
+            line-height: 3rem;
+            height: 100%;
+            font-size: 1.2rem;
+            color: #ee3b24;
+          `}
+        >
+          -{transaction.transactionBalance}
+        </div>
+      </div>
+    ))
   );
 }
 
--- frontend/cardmore/src/pages/MyPage.jsx
+++ frontend/cardmore/src/pages/MyPage.jsx
@@ -1,83 +1,124 @@
 import { css } from "@emotion/css";
 import { useEffect, useState, useRef } from "react";
-import Chart from 'chart.js/auto';
+import Chart from "chart.js/auto";
 import ExpenditureCard from "../components/ExpenditureCard";
 import NavBar from "../components/NavBar";
+import Select from "react-select";
+
+import { getTransactionAll } from "../apis/Mypage";
 
 function MyPage() {
   const chartRef = useRef(null);
+  const chartInstance = useRef(null);
 
-  const data = {
-    labels: [
-      'Red',
-      'Blue',
-      'Yellow'
-    ],
-    datasets: [{
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)'
-      ],
-      hoverOffset: 4
-    }]
+  const [transaction, setTransaction] = useState(null);
+  const [selectedCardIndex, setSelectedCardIndex] = useState(0);
+  const [cardNameList, setSelectedCardNameList] = useState([{ value: 0, label: "전체" }]);
+
+  const [data, setData] = useState(null);
+
+  const getTransaction = async () => {
+    const card = await getTransactionAll();
+    setTransaction(card);
   };
 
   useEffect(() => {
-    const ctx = chartRef.current.getContext('2d');
-    const myChart = new Chart(ctx, {
-      type: 'doughnut',
+    const ctx = chartRef.current.getContext("2d");
+    chartInstance.current = new Chart(ctx, {
+      type: "doughnut",
       data: data,
       options: {
         responsive: true,
         plugins: {
           legend: {
-            position: 'bottom',
+            position: "bottom",
           },
-        }
-      }
+        },
+      },
     });
+
+    getTransaction();
 
     // 컴포넌트 언마운트 시 차트를 삭제
     return () => {
-      myChart.destroy();
+      chartInstance.current.destroy();
     };
   }, []);
 
+  //결제 내역
+  useEffect(() => {
+    if (transaction !== null) {
+      setSelectedCardNameList(
+        transaction.result.cardNameList.map((name, index) => {
+          return {
+            value: index,
+            label: name,
+          };
+        })
+      );
+
+      const newData = {
+        labels: transaction.result.categoryList.map((category) => category.name),
+        datasets: [
+          {
+            label: "소비 금액",
+            data: transaction.result.categoryList.map((category) => category.balance),
+            backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
+            hoverOffset: 4,
+          },
+        ],
+      };
+
+      setData(newData);
+    }
+  }, [transaction]);
+
+  //차트 수정
+  useEffect(() => {
+    chartInstance.current.data = data;
+    chartInstance.current.update();
+  }, [data]);
+
   return (
-    <div className={css`
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: center;
-      height: 100vh;
-      width: 100%;
-      background-color: #F6F6F6;
-    `}>
-      <div className={css`
+    <div
+      className={css`
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        height: 100vh;
+        width: 100%;
+        background-color: #f6f6f6;
+      `}
+    >
+      <div
+        className={css`
           display: flex;
           float: left;
           font-size: 2.5rem;
           padding-top: 5%;
           padding-right: 40%;
           font-weight: bold;
-      `}>My Page</div>
-      
+        `}
+      >
+        My Page
+      </div>
+
       {/* 도넛 차트 */}
-      <div className={css`
+      <div
+        className={css`
           position: absolute;
           top: 13vh;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
-      `}>
-        <canvas 
-          ref={chartRef} 
-          width="270" 
+        `}
+      >
+        <canvas
+          ref={chartRef}
+          width="270"
           height="270"
           className={css`
             width: 260px;
@@ -85,19 +126,26 @@ function MyPage() {
           `}
         />
       </div>
-      <div className={css`
+      <div
+        className={css`
           position: absolute;
           top: 8.5vh;
           display: flex;
           flex-direction: column;
           align-items: flex-end;
-        `}>
-          <p className={css`
+        `}
+      >
+        <p
+          className={css`
             padding: 10px;
             margin: 0;
             font-size: 0.8rem;
-          `}>소비패턴을 기반으로 카드를 추천 받으시겠어요?</p>
-          <button className={css`
+          `}
+        >
+          소비패턴을 기반으로 카드를 추천 받으시겠어요?
+        </p>
+        <button
+          className={css`
             display: flex;
             border-radius: 0.5rem;
             border-style: none;
@@ -109,11 +157,15 @@ function MyPage() {
             font-size: 0.8rem;
             padding: 5px;
             background-color: #555555;
-          `}>카드 추천 받기</button>
-        </div>
-      
+          `}
+        >
+          카드 추천 받기
+        </button>
+      </div>
+
       {/* 지출 내역 */}
-      <div className={css`
+      <div
+        className={css`
           position: absolute;
           display: flex;
           flex-direction: column;
@@ -127,62 +179,118 @@ function MyPage() {
           padding: 1.5rem 2rem;
           box-sizing: border-box;
           box-shadow: 0 -4px 10px rgb(0 0 0 / 10%);
-      `}>
-        <div className={css`
-          display: flex;
-          flex-direction: column;
-          width: 100%;
-          justify-content: center;
-          align-items: flex-start;
-          margin-bottom: 1.5rem;
-        `}>
-          <select name="cars" id="cars" className={css`
+        `}
+      >
+        <div
+          className={css`
             display: flex;
-            border-radius: 1rem;
-            padding: 5px 10px;
-            color: #979797;
-            border: none;
-            background: #F6F6F6;
-            font-weight: 700;
-            font-size: 0.9rem;
-            width: 13rem;
-            box-shadow: 0 4px 5px rgb(0 0 0 / 10%);
-            outline: none;
-          `}>
-            <option value="volvo" className={css`
-              background-color: #ffffff;
-              color: #979797; 
-              font-size: 0.9rem; 
-              padding: 5px;
+            flex-direction: column;
+            width: 100%;
+            justify-content: center;
+            align-items: flex-start;
+            margin-bottom: 1.5rem;
+          `}
+        >
+          <div
+            className={css`
+              width: 10rem;
+            `}
+          >
+            <Select
+              className="basic-single"
+              classNamePrefix="select"
+              defaultValue={cardNameList[0]}
+              isDisabled={false}
+              isLoading={false}
+              isClearable={false}
+              isRtl={false}
+              isSearchable={false}
+              name="color"
+              options={cardNameList}
+              onChange={(e) => setSelectedCardIndex(e.value)}
+            />
+          </div>
+
+          {/* <select
+            name="cars"
+            id="cars"
+            onChange={handleSelectChange}
+            className={css`
+              display: flex;
+              border-radius: 1rem;
+              padding: 5px 10px;
+              color: #979797;
               border: none;
-            `}>전체</option>
-          </select>
+              background: #f6f6f6;
+              font-weight: 700;
+              font-size: 0.9rem;
+              width: 13rem;
+              box-shadow: 0 4px 5px rgb(0 0 0 / 10%);
+              outline: none;
+            `}
+          >
+            {transaction !== null ? (
+              transaction.result.cardNameList.map((name, index) => (
+                <option
+                  key={index}
+                  value={index}
+                  className={css`
+                    background-color: #ffffff;
+                    color: #979797;
+                    font-size: 0.9rem;
+                    padding: 5px;
+                    border: none;
+                  `}
+                >
+                  {name}
+                </option>
+              ))
+            ) : (
+              <option
+                value="volvo"
+                className={css`
+                  background-color: #ffffff;
+                  color: #979797;
+                  font-size: 0.9rem;
+                  padding: 5px;
+                  border: none;
+                `}
+              >
+                전체
+              </option>
+            )}
+          </select> */}
         </div>
-        <div className={css`
-          display: flex;
-          flex-direction: column;
-          width: 100%;
-          overflow-y: scroll;
+        <div
+          className={css`
+            display: flex;
+            flex-direction: column;
+            width: 100%;
+            overflow-y: scroll;
 
-          ::-webkit-scrollbar {
-            width: 8px;
-          }
+            ::-webkit-scrollbar {
+              width: 8px;
+            }
 
-          ::-webkit-scrollbar-thumb {
-            background: rgb(0, 0, 0, 0.1);
-            border-radius: 4px;
-          }
+            ::-webkit-scrollbar-thumb {
+              background: rgb(0, 0, 0, 0.1);
+              border-radius: 4px;
+            }
 
-          ::-webkit-scrollbar-thumb:hover {
-            background: rgb(0, 0, 0, 0.4);
-          }
-        `}>
-          <ExpenditureCard />
-          <ExpenditureCard />
-          {/* 더 많은 ExpenditureCard 추가 */}
+            ::-webkit-scrollbar-thumb:hover {
+              background: rgb(0, 0, 0, 0.4);
+            }
+          `}
+        >
+          {/* Props로 데이터 넘겨주기 */}
+          <ExpenditureCard
+            transactionList={
+              transaction !== null ? transaction.result.transactionList[selectedCardIndex] : []
+            }
+          />
         </div>
       </div>
-      <NavBar isSelected={"User"}/>
+      <NavBar isSelected={"User"} />
     </div>
   );
 }
