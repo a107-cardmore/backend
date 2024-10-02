41628ec68a3d85407eaec9b44274364f6a6304fa
dahyunko <kdhgby@naver.com>
2024-10-02 15:01:18 +0900

    feat: discount info finished

--- backend/src/main/java/a107/cardmore/domain/user/controller/UserController.java
+++ backend/src/main/java/a107/cardmore/domain/user/controller/UserController.java
@@ -1,7 +1,10 @@
 package a107.cardmore.domain.user.controller;
 
 import a107.cardmore.domain.user.service.UserService;
+import a107.cardmore.util.base.BaseSuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @GetMapping
+    public BaseSuccessResponse<String> userNickName(){
+        String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
+        return new BaseSuccessResponse<>(userService.userNickName(userEmail));
+    }
 }
