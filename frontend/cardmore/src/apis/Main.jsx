62fc9c63f313147da61f2e455803f10aa4f96517
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-10-02 14:21:09 +0900

    fix: 비동기 처리

--- frontend/cardmore/src/apis/Main.jsx
+++ frontend/cardmore/src/apis/Main.jsx
@@ -2,7 +2,7 @@ import axios from "./Axios";
 
 export const getCards = async () => {
   try {
-    const response = axios.get("/cards").then((res) => {
+    const response = await axios.get("/cards").then((res) => {
       return res.data;
     });
     console.log("[IN AXIOS] get Cards", response);
--- frontend/cardmore/src/apis/Mypage.jsx
+++ frontend/cardmore/src/apis/Mypage.jsx
@@ -2,7 +2,7 @@ import axios from "./Axios";
 
 export const getTransactionAll = async () => {
   try {
-    const response = axios.get("/transactions").then((res) => {
+    const response = await axios.get("/transactions").then((res) => {
       return res.data;
     });
     console.log("[IN AXIOS] mypage get transaction all", response);
